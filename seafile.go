b00feaec89b33d2c3b3df01df3c4b24876c503a2
bartmeuris <bart.meuris@gmail.com>
2015-01-13 16:54:31 +0100

    Various improvements

--- seafile.go
+++ seafile.go
@@ -1,19 +1,18 @@
 package goseafile
 
-
 import (
-	"net/http"
-	"net/url"
 	"encoding/json"
-	"log"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
 	"strings"
 )
 
 type SeaFile struct {
 	AuthToken string
-	Url string
+	Url       string
 }
 
 func (s *SeaFile) newReq(method, entry string) (*http.Request, error) {
@@ -28,8 +27,8 @@ func (s *SeaFile) newReq(method, entry string) (*http.Request, error) {
 		return nil, err
 	} else {
 		req.Header.Add("Accept", "application/json")
-		if (s.AuthToken != "") {
-			req.Header.Add("Authorization", "Token " + s.AuthToken)
+		if s.AuthToken != "" {
+			req.Header.Add("Authorization", "Token "+s.AuthToken)
 		}
 		req.ParseForm()
 		return req, nil
@@ -38,7 +37,7 @@ func (s *SeaFile) newReq(method, entry string) (*http.Request, error) {
 func (s *SeaFile) reqResp(method, fnc string, form url.Values) (*http.Response, error) {
 	if req, err := s.newReq(method, fnc); err != nil {
 		return nil, err
-	} else if (req == nil ) {
+	} else if req == nil {
 		return nil, fmt.Errorf("request nil")
 	} else {
 		if form != nil {
@@ -59,27 +58,31 @@ func (s *SeaFile) req(method, fnc string, form url.Values, rv interface{}) error
 	} else {
 		defer resp.Body.Close()
 		if resp.StatusCode != 200 {
-			return fmt.Errorf("Request error: got status %d", resp.StatusCode)
+			if body, err := ioutil.ReadAll(resp.Body); err == nil {
+				log.Printf("Request: %s %s\n", method, fnc)
+				log.Printf("Body:\n---\n%s\n---\n", string(body))
+			}
+			return fmt.Errorf("request error: got status %d", resp.StatusCode)
 		}
-		if (rv != nil) {
+		if rv != nil {
 			dec := json.NewDecoder(resp.Body)
 			if err := dec.Decode(rv); err != nil {
 				return err
 			}
 			/*
-			// For debug purposes
-			if body, err := ioutil.ReadAll(resp.Body); err != nil {
-				return err
-			} else {
-				rd := strings.NewReader(string(body))
-				//dec := json.NewDecoder(resp.Body)
-				dec := json.NewDecoder(rd)
-				if err := dec.Decode(rv); err != nil {
-					log.Printf("Error decoding body: %s\n", err)
-					log.Printf("Body:\n----\n%s\n----\n", string(body))
+				// For debug purposes
+				if body, err := ioutil.ReadAll(resp.Body); err != nil {
 					return err
+				} else {
+					rd := strings.NewReader(string(body))
+					//dec := json.NewDecoder(resp.Body)
+					dec := json.NewDecoder(rd)
+					if err := dec.Decode(rv); err != nil {
+						log.Printf("Error decoding body: %s\n", err)
+						log.Printf("Body:\n----\n%s\n----\n", string(body))
+						return err
+					}
 				}
-			}
 			*/
 		}
 		return nil
@@ -89,10 +92,6 @@ func (s *SeaFile) req(method, fnc string, form url.Values, rv interface{}) error
 func (s *SeaFile) Login(user string, password string) error {
 	var tok struct {
 		Token string
-	}
-	if !s.Ping() {
-		// Prevent sending credentials to a wrong url
-		return fmt.Errorf("server did not respond correctly to ping")
 	}
 	v := url.Values{
 		"username": {user},
@@ -119,13 +118,26 @@ func (s *SeaFile) Authed() bool {
 }
 
 func (s *SeaFile) Ping() bool {
-	var rv string
-	if err := s.req("GET", "/ping/", nil, &rv); err != nil {
-		log.Printf("ERROR: ping failed: %s\n", err)
+	if resp, err := s.reqResp("GET", "/ping/", nil); err != nil {
 		return false
-	} else if rv != "pong" {
-		return false
+	} else {
+		defer resp.Body.Close()
+		// Ping is throttled, and can get a status 429 back. This is interpreted as "bad"
+		if resp.StatusCode == 429 {
+			// Valid, but throttled.
+			return true
+		} else if resp.StatusCode == 200 {
+			// Output should be "pong"
+			if b, err := ioutil.ReadAll(resp.Body); err != nil {
+				return false
+			} else if string(b) == "\"pong\"" {
+				return true
+			} else {
+				log.Printf("ERROR: Unexpected value from ping: '%s'\n", string(b))
+			}
+		} else {
+			log.Printf("ERROR: Unknown status code as response to ping: %d\n", resp.StatusCode)
+		}
 	}
-	return true
+	return false
 }
-
