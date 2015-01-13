21735b827c537d49a677a6945ac7695f0fc385cb
bartmeuris <bart.meuris@gmail.com>
2015-01-13 16:53:00 +0100

    Stream upload fixed

--- library.go
+++ library.go
@@ -1,29 +1,28 @@
 package goseafile
 
 import (
-	"log"
-	"io"
 	"fmt"
+	"io"
 	"io/ioutil"
-	"net/http"
 	"mime/multipart"
+	"net/http"
+	"net/url"
 	"path/filepath"
 )
 
 type Library struct {
-	sf *SeaFile `json:"-"`
+	sf         *SeaFile `json:"-"`
 	Permission string
-	Encrypted bool
-	Mtime int64
-	Owner string
-	Id string
-	Size int
-	Name string
-	Virtual bool
-	Desc string
-	Root string
+	Encrypted  bool
+	Mtime      int64
+	Owner      string
+	Id         string
+	Size       int
+	Name       string
+	Virtual    bool
+	Desc       string
+	Root       string
 }
-
 
 func (s *SeaFile) GetLibrary(lib string) (*Library, error) {
 	if libl, err := s.ListLibraries(); err != nil {
@@ -49,31 +48,45 @@ func (s *SeaFile) ListLibraries() ([]*Library, error) {
 	return v[0:], nil
 }
 
+func NewLibrary(seafile *SeaFile, id string) *Library {
+	lib := &Library{
+		Id: id,
+		sf: seafile,
+	}
+	if err := lib.Update(); err != nil {
+		return nil
+	}
+	return lib
+}
+
 func (l *Library) GetOwner() string {
 	var own struct {
 		Owner string
 	}
-	if err := l.sf.req("GET", "/repos/" + l.Id + "/owner/", nil, &own); err != nil {
+	if err := l.sf.req("GET", "/repos/"+l.Id+"/owner/", nil, &own); err != nil {
 		return ""
+	} else {
+		return own.Owner
 	}
-	return own.Owner
 }
 
-
-func streamUpload(w *io.PipeWriter, r io.Reader, filename, fieldname string, params map[string]string ) error {
+// upload with a pipewriter -> stream upload
+func streamUpload(f io.Reader, filename, fieldname string, params map[string]string) (string, *io.PipeReader, error) {
 	// First handle closable resources
-	rc, ok := r.(io.ReadCloser)
+	r, w := io.Pipe()
+	rc, ok := f.(io.ReadCloser)
 	if !ok && r != nil {
 		rc = ioutil.NopCloser(r)
 	}
-
-	log.Printf("Creating multipart writer...\n")
 	writer := multipart.NewWriter(w)
-	// Now write the file
+	ctype := writer.FormDataContentType()
 	go func() {
+		// This runs in background and writes to the PipeWriter
+		// which blocks until something is read from the returned PipeReader
+		// This allows the streaming to be efficient and prevents loading
+		// the entire file in memory.
 		defer rc.Close()
 		defer writer.Close()
-		log.Printf("Creating and uploading formfile...\n")
 		if pw, err := writer.CreateFormFile(fieldname, filename); err != nil {
 			w.CloseWithError(err)
 			return
@@ -81,29 +94,26 @@ func streamUpload(w *io.PipeWriter, r io.Reader, filename, fieldname string, par
 			w.CloseWithError(err)
 			return
 		}
-		log.Printf("Adding Parameters...\n")
 		// Write the parameters
 		for key, val := range params {
 			writer.WriteField(key, val)
 		}
-		log.Printf("Uploading done.\n")
 		w.Close()
 	}()
-	return nil
+	return ctype, r, nil
 }
 
-func (l *Library) Upload(path string, file io.Reader) error {
+func (l *Library) Upload(path string, fileio io.Reader) error {
 	// http://manual.seafile.com/develop/web_api.html#upload-file
 	// 1. Get upload url
 	var upllink string
-	if err := l.sf.req("GET", "/repos/" + l.Id + "/upload-link/", nil, &upllink); err != nil {
+	if err := l.sf.req("GET", "/repos/"+l.Id+"/upload-link/", nil, &upllink); err != nil {
 		return err
 	}
 
 	// 2 - upload the file
 	// https://github.com/gebi/go-fileupload-example/blob/master/main.go
 	// http://matt.aimonetti.net/posts/2013/07/01/golang-multipart-file-upload-example/
-	log.Printf("Uploading file to: %s\n", upllink)
 	if req, err := l.sf.newReq("POST", upllink); err != nil {
 		return err
 	} else {
@@ -113,22 +123,45 @@ func (l *Library) Upload(path string, file io.Reader) error {
 		if path == "" {
 			path = "/"
 		}
-
-		// Continue with upload!
-		log.Printf("Creating Pipe...\n")
-		r,w := io.Pipe()
-		if err := streamUpload(w, file, fn, "file", map[string]string{ "parent_dir": path, "filename": fn }); err != nil {
-			return err
+		formval := map[string]string{
+			"parent_dir": path,
+			"filename":   fn,
 		}
-		req.Body = r
-		// Now send the request
-		log.Printf("Sending request...\n")
-		if resp, err := http.DefaultClient.Do(req); err != nil {
+		if ctype, r, err := streamUpload(fileio, fn, "file", formval); err != nil {
 			return err
 		} else {
-			log.Printf("Response status: %s\n", resp.Status)
+			req.Body = r
+			req.Header.Set("Content-Type", ctype)
+			// Now send the request
+			if resp, err := http.DefaultClient.Do(req); err != nil {
+				return err
+			} else if resp.Status != "200 OK" {
+				return fmt.Errorf("expected return status '200 OK', got '%s'", resp.Status)
+			}
 		}
 	}
 	return nil
 }
 
+func (l *Library) List(path string) ([]File, error) {
+	var flist []File
+	urls := "/repos/" + l.Id + "/dir/"
+	if path != "" {
+		urls = urls + "?p=" + url.QueryEscape(path)
+	}
+	if err := l.sf.req("GET", urls, nil, &flist); err != nil {
+		return nil, err
+	} else {
+		for i, _ := range flist {
+			flist[i].lib = l
+		}
+		return flist, nil
+	}
+}
+
+func (l *Library) Update() error {
+	if err := l.sf.req("GET", "/repos/"+l.Id+"/", nil, l); err != nil {
+		return err
+	}
+	return nil
+}
